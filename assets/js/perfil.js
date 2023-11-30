de1462a3ef5db7c86787cd5bf89850aac4bb4110
Juan David Arcila <j.arcila1@utp.edu.co>
2023-11-30 18:11:47 -0500

    cambio en los botones de la pagina de perfil

--- assets/js/perfil.js
+++ assets/js/perfil.js
@@ -15,21 +15,15 @@ function cerrarSesion() {
 }
 
 const contenido = document.querySelector("#cardGroup");
-const fragment = document.createDocumentFragment(); 
+const fragment = document.createDocumentFragment();
 
 function actualizarHTML(estado) {
-for (let i = 0; i < sessionStorage.length; i++) {
-  const key = sessionStorage.key(i);
-  if(key !== "usuarios" && key !== "usuarioActual"){
-    const tutorialesGuardados = JSON.parse(sessionStorage.getItem(key)) || [];
-    console.log("1 ",key, tutorialesGuardados);
+  for (let i = 0; i < sessionStorage.length; i++) {
+    const key = sessionStorage.key(i);
+    if (key !== "usuarios" && key !== "usuarioActual") {
+      const tutorialesGuardados = JSON.parse(sessionStorage.getItem(key)) || [];
 
-    
-      console.log("2 ", key, tutorialesGuardados);
-
-      
       if (estado === "Guardado" && tutorialesGuardados.estado === "Guardado") {
-        console.log("paso guardado ", key, tutorialesGuardados);
         const card = document.createElement("div");
         card.className = "col";
         card.innerHTML = `
@@ -39,76 +33,55 @@ for (let i = 0; i < sessionStorage.length; i++) {
               <iframe loading="lazy" type="text/html" width="100%" height="280" src="${key}" allowfullscreen frameborder="0"></iframe>
             </div>
             <div class="card-footer">
-              <button type="button" class="tutorial-button btn btn-warning" onclick="eliminarTutorial()" data-tutorial-state="Guardado">
+              <button type="button" class="tutorial-button btn btn-warning" onclick="eliminarTutorial('${key}')" data-tutorial-state="Guardado">
                 <i class="bi bi-bookmark"></i>
-              </button>
-              <button type="button" class="btn btn-primary tutorial-button" data-tutorial-state="Pendiente">
-                <i class="bi bi-play-circle"></i>
-              </button>
-              <button type="button" class="btn btn-danger tutorial-button" data-tutorial-state="VerMastarde">
-                <i class="bi bi-clock"></i>
               </button>
             </div>
           </div>
         `;
         fragment.appendChild(card);
-    } else if (estado === "Pendiente" && tutorialesGuardados.estado === "Pendiente") {
-      console.log("paso pendiente" , key, tutorialesGuardados);
-      const card = document.createElement("div");
-      card.className = "col";
-      card.innerHTML = `
+      } else if (
+        estado === "Pendiente" &&
+        tutorialesGuardados.estado === "Pendiente"
+      ) {
+        const card = document.createElement("div");
+        card.className = "col";
+        card.innerHTML = `
         <div class="card-tuto m-2">
           <div class="card-body">
             <h5 class="card-title">${tutorialesGuardados.titulo}</h5>
             <iframe loading="lazy" type="text/html" width="100%" height="280" src="${key}" allowfullscreen frameborder="0"></iframe>
           </div>
           <div class="card-footer">
-            <button type="button" class="tutorial-button btn btn-warning " data-tutorial-state="Guardado">
-              <i class="bi bi-bookmark"></i>
-            </button>
             <button type="button" class="btn btn-primary tutorial-button" data-tutorial-state="Pendiente">
               <i class="bi bi-play-circle"></i>
-            </button>
-            <button type="button" class="btn btn-danger tutorial-button" data-tutorial-state="VerMastarde">
-              <i class="bi bi-clock"></i>
             </button>
           </div>
         </div>
       `;
-      fragment.appendChild(card);
-    } else if (estado === "VerMastarde" && tutorialesGuardados.estado === "VerMastarde") {
-      console.log("paso mastarde", key, tutorialesGuardados);
-      const card = document.createElement("div");
-      card.className = "col";
-      card.innerHTML = `
+        fragment.appendChild(card);
+      } else if (
+        estado === "VerMastarde" &&
+        tutorialesGuardados.estado === "VerMastarde"
+      ) {
+        const card = document.createElement("div");
+        card.className = "col";
+        card.innerHTML = `
         <div class="card-tuto m-2">
           <div class="card-body">
             <h5 class="card-title">${tutorialesGuardados.titulo}</h5>
             <iframe loading="lazy" type="text/html" width="100%" height="280" src="${key}" allowfullscreen frameborder="0"></iframe>
           </div>
           <div class="card-footer">
-            <button type="button" class="tutorial-button btn btn-warning " data-tutorial-state="Guardado">
-              <i class="bi bi-bookmark"></i>
-            </button>
-            <button type="button" class="btn btn-primary tutorial-button" data-tutorial-state="Pendiente">
-              <i class="bi bi-play-circle"></i>
-            </button>
             <button type="button" class="btn btn-danger tutorial-button" data-tutorial-state="VerMastarde">
               <i class="bi bi-clock"></i>
             </button>
           </div>
         </div>
       `;
-      fragment.appendChild(card);
+        fragment.appendChild(card);
+      }
+      contenido.appendChild(fragment);
     }
-    contenido.appendChild(fragment);
-
-  };
   }
-
-  function eliminarTutorial(clickedElement) {
-    sessionStorage.removeItem(tutorialId);
-    actualizarHTML();
-  }
-
 }
